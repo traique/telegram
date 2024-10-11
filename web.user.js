20622ac69baf75dd9ed73e055cbaa6ea8a55d2f6
traique <95959617+traique@users.noreply.github.com>
2024-10-11 12:14:30 +0700

    web.user.js

--- web.user.js
+++ web.user.js
@@ -6,8 +6,8 @@
 // @match        https://*/*
 // @grant        none
 // @icon         https://your-icon-link-here.png
-// @downloadURL  https://github.com/traique/telegram/raw/main/web.user.js
-// @updateURL    https://github.com/traique/telegram/raw/main/web.user.js
+// @downloadURL  https://github.com/traique/telegram/blob/main/web.user.js
+// @updateURL    https://github.com/traique/telegram/blob/main/web.user.js
 // @homepage     https://github.com/traique/telegram
 // ==/UserScript==
 
