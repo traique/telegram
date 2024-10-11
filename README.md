bcaaa593acd54d3b12e59529a7ddc83f8a10e82d
traique <95959617+traique@users.noreply.github.com>
2024-10-11 11:48:07 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+> [!NOTE]
+> - Contacts: [Telegram](https://t.me/traique007)
+ 
+
+---
+## How to run  
+- Install extensions in your browser[Violentmonkey](https://chromewebstore.google.com/detail/violentmonkey/jinjaccalgkegednnccohejagnlnfdag?hl=be)
+- In order for **bot** to open in a browser, install [this script](https://github.com/traique/telegram/raw/main/web.user.js)
+- Open Bot and run the game
+
+
+---
---
+++ web.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         web UI
+// @version      1.0
+// @description  Running BOT in a browser
+// @author       traique
+// @match        none
+// @grant        none
+// @icon         none
+// @downloadURL  https://github.com/traique/telegram/raw/main/web.user.js
+// @updateURL    https://github.com/traique/telegram/raw/main/web.user.js
+// @homepage     https://github.com/traique
+// ==/UserScript==
+
+(function() {
+  function updateIframeSrc() {
+    const iframes = document.querySelectorAll('iframe'); // Chọn tất cả iframe
+
+    iframes.forEach(iframe => {
+      let src = iframe.src;
+
+      if (src.includes('tgWebAppPlatform=weba') || src.includes('tgWebAppPlatform=web')) {
+        src = src.replace(/tgWebAppPlatform=(weba|web)/g, 'tgWebAppPlatform=ios');
+
+        iframe.src = src;
+
+        console.log('Đường dẫn đã được cập nhật:', src);
+      }
+    });
+  }
+
+  setInterval(updateIframeSrc, 2000);
+})();
